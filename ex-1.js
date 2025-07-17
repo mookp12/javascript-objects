8c3d3237bd0676de0225499053dd7b1164bc0830
plawan1995 <jnyjame12@gmail.com>
2025-07-17 13:35:00 +0700

    feat: enhance food order and product details in exercises

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,12 @@
 // Exercise #1: Food Order
-
+const foodOrder ={
+    orderNumber :"A0234",
+    address : "Bangkok",
+    paymentType : "Cash",
+    totalPrice : "4500",
+    restaurantName :"MK"
+}
 // Start coding here
+foodOrder.totalPrice = 500;
+foodOrder.paymentType = "Credit Card"
+console.log(foodOrder);
